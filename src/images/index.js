1687d9537d6a19723dc04b198859ff6ef54189db
Pavel Muller <pavel.muller@jbknowledge.com>
2017-10-03 09:04:42 -0300

    Move icons to public folder

--- src/images/index.js
+++ src/images/index.js
@@ -1,11 +1,9 @@
 import microphone from './microphone.svg'
 import draw from './draw.svg'
 import theatre from './theatre.svg'
-import * as favicons from './favicons'
 
 export {
   microphone,
   draw,
   theatre,
-  favicons
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
-import favicons from './images'
 import './index.css'
 import registerServiceWorker from './registerServiceWorker';
 
