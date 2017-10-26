9e19141622b79daf3a8d5f06bd0bf75dffe9f450
Pavel Muller <pavel.muller@jbknowledge.com>
2017-10-26 16:11:25 -0300

    Add czech rules translation

--- src/locale/cs.js
+++ src/locale/cs.js
@@ -18,15 +18,15 @@ const cs = {
     cs: "Čeština",
     rules: {
       title: "Pravidla",
-      game: "Groups of 2 or more people are trying to guess the given words and win specific amount of points. First group will choose one of their players - representative, in order to explain the given word. In the following rounds, all the players within the teams will be replaced in that role. The representative will choose the activity:",
-      speaking: "The relevant word, its parts or the words derived from it must not be used.",
-      drawing: "Must not speak and the drawing must not contain numbers or letters.",
-      pantomima: "Must not speak, make any sounds nor use any objects.",
-      level: "Then the representative will choose the difficulty:",
-      easy: "For 1 point.",
-      intermediate: "For 2 points.",
-      difficult: "For 3 points.",
-      course: "The representative will remember the word that appears on the screen. Will press the 'Start' button and 60 seconds counting will start. The representative will try to explain the word as close as possible to his groupmates. They can yell out loud what they think the word might be. If someone from the group guesses the exact word within a given time limit, the group will take the points according to the difficulty. If not, no points will be added.",
+      game: "Skupina o dvou a více lidech se snaží uhádnout dané slovo a tak vyhrát určitý počet bodů. První skupina vzbere jednoho z jejich hráčů - zástupce, aby vysvětlil dané slovo. V následujících kolech se v této pozici postupně vystřídají všichni hráči skupiny. Zástupce vybere aktivitu:",
+      speaking: "Nesmí se použít dané slovo, jeho části nebo odvozené slova.",
+      drawing: "Nesmí se mluvit a kresba nesmí obsahovat čísla nebo znaky.",
+      pantomima: "Nesmí se mluvit, vydávat zvuky ani používat objekty.",
+      level: "Poté si zástupce vybere obtížnost:",
+      easy: "Za 1 bod.",
+      intermediate: "Za 2 body.",
+      difficult: "Za 3 body.",
+      course: "Zástupce si zapamatuje slovo které se objeví na obrazovce. Stiskne tlačítko 'Start a tím začne 60ti sekundový odpočet. Zástupce se snaží co nejlépe vysvětlit dané slovo svým spoluhráčům. Ti mohou vykřikovat co si myslí že by dané slovo mohlo být. Pokud někdo ze skupiny přesně uhádne dané slovo během daného časového limitu, skupina obdrží body podle vybrané obtížnosti. Pokud ne, žádné body se nepřidají.",
     }
   }
 }
